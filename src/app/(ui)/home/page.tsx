539634c6ebde4316bc3a5a6ad47809374475405f
Helsi <helsi_@hotmail.com>
2025-04-11 09:16:16 -0400

    caixa de criar tweets

--- src/app/(ui)/home/page.tsx
+++ src/app/(ui)/home/page.tsx
@@ -1,9 +1,11 @@
 import { HomeHeader } from "@/components/home/home-header"
+import { TweetPost } from "@/components/tweet/tweet-post"
 
 const Page = () => {
     return (
         <div className="">
             <HomeHeader />
+            <TweetPost />
         </div>
     )
 }
